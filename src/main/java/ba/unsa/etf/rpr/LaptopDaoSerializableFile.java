cdad65432979bcaa35415dd2df26d4b10c29b1dc
Emir Kapić <73301880+EmirKapic@users.noreply.github.com>
2022-11-07 12:45:23 +0100

    SerializableDao fix

--- src/main/java/ba/unsa/etf/rpr/LaptopDaoSerializableFile.java
+++ src/main/java/ba/unsa/etf/rpr/LaptopDaoSerializableFile.java
@@ -4,63 +4,62 @@ import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class LaptopDaoSerializableFile implements LaptopDao{
-    private File file;
-    private ArrayList<Laptop> laptopi;
-    private FileOutputStream fos;
-    private ObjectOutputStream oos;
+    public class LaptopDaoSerializableFile implements LaptopDao{
+        private File file;
+        private ArrayList<Laptop> laptopi;
+        private ObjectOutputStream oos;
 
-    public LaptopDaoSerializableFile() throws IOException {
-        file = new File("laptops.txt");
-        laptopi = new ArrayList<>();
-        fos = new FileOutputStream(file.getCanonicalPath(), true);
-        oos = new ObjectOutputStream(fos);
-        file.createNewFile();
-    }
+        private void save() throws Exception {
+            ObjectOutputStream ois = new ObjectOutputStream(new FileOutputStream(file));
+            ois.writeObject(laptopi);
+        }
 
-    @Override
-    public void dodajLaptopUListu(Laptop laptop) {
-        laptopi.add(laptop);
-    }
+        public LaptopDaoSerializableFile() throws IOException {
+            this.file = new File("laptops.txt");
+            this.laptopi = new ArrayList<Laptop>();
+            this.oos = new ObjectOutputStream(new FileOutputStream(file));
+        }
 
-    @Override
-    public void dodajLaptopUFile(Laptop laptop) throws IOException {
-        oos.writeObject(laptop);
-    }
+        @Override
+        public void dodajLaptopUListu(Laptop laptop) {
+            this.laptopi.add(laptop);
+        }
 
-    /**
-     *
-     * @param procesor name of the processor
-     * @return laptop with the given processor, or null if it doesn't exist
-     */
-    @Override
-    public Laptop getLaptop(String procesor) {
-        for (Laptop l : laptopi)
-            if (l.getProcesor().equals(procesor))
-                return l;
-        return null;
-    }
-
-    @Override
-    public void napuniListu(ArrayList<Laptop> laptopi) {
-        this.laptopi.addAll(laptopi);
-    }
-
-    @Override
-    public ArrayList<Laptop> vratiPodatkeIzDatoteke() throws IOException, ClassNotFoundException {
-        FileInputStream fis = new FileInputStream(file.getCanonicalPath());
-        ObjectInputStream f = new ObjectInputStream(fis);
-        ArrayList<Laptop> l = new ArrayList<>();
-        while (true) {
-            Laptop lapt = new Laptop();
+        @Override
+        public void dodajLaptopUFile(Laptop laptop) throws IOException {
+            dodajLaptopUListu(laptop);
             try {
-                lapt = (Laptop) f.readObject();
-                l.add(lapt);
-            } catch(Exception e){
-                System.out.println(e);
-                break;
+                this.save();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-        return l;
+
+        /**
+         *
+         * @param procesor name of the processor
+         * @return laptop with the given processor, or null if it doesn't exist
+         */
+        @Override
+        public Laptop getLaptop(String procesor) {
+            for (Laptop l : laptopi)
+                if (l.getProcesor().equals(procesor))
+                    return l;
+            return null;
+        }
+
+        @Override
+        public void napuniListu(ArrayList<Laptop> laptopi) {
+            this.laptopi.addAll(laptopi);
+        }
+
+        @Override
+        public ArrayList<Laptop> vratiPodatkeIzDatoteke() throws IOException, ClassNotFoundException {
+            ArrayList<Laptop> rez = new ArrayList<Laptop>();
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+            rez = (ArrayList<Laptop>) ois.readObject();
+            return rez;
+        }
     }
-}
+
+
